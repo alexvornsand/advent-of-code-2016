77eee38005e063f30c55592ff2735ba314555bb5
alexvornsand <29985111+alexvornsand@users.noreply.github.com>
2023-12-06 15:39:33 -0600

    Update day-07.py

--- solutions/day-07/day-07.py
+++ solutions/day-07/day-07.py
@@ -3,28 +3,32 @@ class TLSAssessor:
         self.ips = ips
 
     def containsABBA(self, string):
-        relevant_string = ['_' for i in range(len(string))]
-        valid_string = False
-        for i in range(len(string) - 3):
-            if string[i] in ['[', ']']:
-                relevant_string[i] = string[i]
-            if string[i] == string[i + 3] and string[i] != string[i + 1] and string[i + 1] == string[i + 2]:
-                relevant_string[i:i + 4] = string[i:i + 4]
-                valid_string = True
-        if valid_string:
-            print(''.join(relevant_string))
-        return valid_string
-#        return any([string[i] == string[i + 3] and string[i] != string[i + 1] and string[i + 1] == string[i + 2] for i in range(len(string) - 3)])
-    
+        return any([string[i] == string[i + 3] and string[i] != string[i + 1] and string[i + 1] == string[i + 2] for i in range(len(string) - 3)])
+
+    def matchesABAtoBAB(self, ip):
+        abas = []
+        for i in range(len(ip) - 2):
+            if ip[i] == ip[i + 2] and ip[i] != ip[i + 1] and '[' not in ip[i:i + 3] and ']' not in ip[i:i + 3]:
+                abas.append(ip[i:i + 3])
+        for aba in list(set(abas)):
+            a, b = [char for char in aba[:2]]
+            bab = ''.join([b, a, b])
+            safe_aba = re.search('(?:\[.*?\])*[^\[]*(' + aba + ').*', ip)
+            safe_bab = re.search('.*?\[.*?(' + bab + ').*\].*', ip)
+            if safe_aba  is not None and safe_bab is not None:
+                relevant = [char for char in re.sub('[a-z]', '_', ip)]
+                print(''.join(relevant))
+                print(safe_aba.start())
+                relevant[safe_aba.start():safe_aba.end()] = [char for char in aba]
+                relevant[safe_bab.start():safe_bab.end()] = [char for char in bab]
+                print(''.join(relevant))
+                return True
+        return False
+
     def meetsTLS(self, ip):
-        print(ip)
         abba = self.containsABBA(ip)
-        abba_in_protected_strings = []
-        for protected_string in list(re.findall('\[.*?\]', ip)):
-            print(protected_string)
-            abba_in_protected_strings.append(self.containsABBA(protected_string))
-        safe_protected_strings = not any(abba_in_protected_strings)
-#        safe_protected_strings = not any([self.containsABBA(segment) for segment in list(re.findall('\[.*\]?', ip))])
-        print(abba, safe_protected_strings)
-        print('\n')
+        safe_protected_strings = not any([self.containsABBA(segment) for segment in list(re.findall('\[.*?\]', ip))])
         return abba and safe_protected_strings
+    
+    def meetsSSL(self, ip):
+        return self.matchesABAtoBAB(ip)
